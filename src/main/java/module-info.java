d419665f335edd5e044e16b7539eb4c31d25693a
Parvez-2211043 <2211043@iub.edu.bd>
2024-12-12 00:13:33 +0600

    modified git ignore, fixed module-info.java, fixed HelloApplication.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,28 +10,24 @@
 //}
 
 module com.example.dairy {
-        requires javafx.controls;
-        requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
 
-        exports com.example.dairy;
-        exports com.example.dairy.Nupur;
+    exports com.example.dairy;
+    exports com.example.dairy.Nupur;
 
-        opens com.example.dairy to javafx.graphics;
-        opens com.example.dairy.Nupur to javafx.fxml;
-        }
+    opens com.example.dairy to javafx.fxml, javafx.graphics;
+    opens com.example.dairy.Nupur to javafx.fxml;
 
-//=======
+    exports com.example.dairy.mahamud;
+    opens com.example.dairy.mahamud to javafx.fxml;
 
-    //opens com.example.dairy to javafx.fxml;
-    //exports com.example.dairy.mahamud;
-    //opens com.example.dairy.mahamud to javafx.fxml;
-    //opens com.example.dairy.Samiul.User7 to javafx.fxml;
-    //opens com.example.dairy.Samiul.User8 to javafx.fxml;
+    opens com.example.dairy.Samiul.User7 to javafx.fxml;
+    opens com.example.dairy.Samiul.User8 to javafx.fxml;
 
-    //exports com.example.dairy;
+    exports com.example.dairy.ParvezHassan;
+    opens com.example.dairy.ParvezHassan to javafx.fxml, javafx.graphics;
+}
 
 
-    //opens com.example.dairy.ParvezHassan to javafx.fxml, javafx.graphics;
-    //exports com.example.dairy.ParvezHassan;
-//}
-//>>>>>>> 9bc02a48fdd84a39b6db8b30a6ba4c38024277de
