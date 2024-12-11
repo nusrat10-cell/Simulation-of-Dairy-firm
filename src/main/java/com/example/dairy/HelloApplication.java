4cde837b161f97ae6bc020fc45b0f2361aa02eda
mahamud-tech <hm129374@gmail.com>
2024-12-11 10:43:30 +0600

    error fixed

--- src/main/java/com/example/dairy/HelloApplication.java
+++ src/main/java/com/example/dairy/HelloApplication.java
@@ -6,12 +6,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/dairy/ParvezHassan/LogIn.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/dairy/mahamud/Start _Collection.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
---
+++ src/main/java/com/example/dairy/mahamud/StartCollectionController.java
@@ -0,0 +1,27 @@
+package com.example.dairy.mahamud;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class StartCollectionController
+{
+    @javafx.fxml.FXML
+    private DatePicker datefx;
+    @javafx.fxml.FXML
+    private TextField farmernamefx;
+    @javafx.fxml.FXML
+    private TextField quantityfx;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void confirmcollectionbuttonfx(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backbuttonfx(ActionEvent actionEvent) {
+    }
+}
