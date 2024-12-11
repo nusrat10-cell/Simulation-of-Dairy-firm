52de4565acb0e5a85f516638d95dd58992e1f2cd
nusrat10-cell <nushratahmedmiky@gmail.com>
2024-12-11 18:12:40 +0600

    error corrected

--- src/main/java/com/example/dairy/HelloApplication.java
+++ src/main/java/com/example/dairy/HelloApplication.java
@@ -6,15 +6,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Nupur/Manage inventory level.fxml"));
 
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/dairy/ParvezHassan/LogIn.fxml"));
+
+
+       // FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/dairy/mahamud/Start _Collection.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
