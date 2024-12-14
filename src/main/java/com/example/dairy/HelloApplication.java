2e0da4e78368c43ac1252775ff9de8cbc2de9046
Parvez-2211043 <2211043@iub.edu.bd>
2024-12-14 22:30:34 +0600

    fixed HelloApplication

--- src/main/java/com/example/dairy/HelloApplication.java
+++ src/main/java/com/example/dairy/HelloApplication.java
@@ -13,34 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
 
 
-<<<<<<< HEAD
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/dairy/Nupur/OrderStatus.fxml"));
-
-        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/dairy/ParvezHassan/LogIn.fxml"));
-=======
-       // FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/dairy/mahamud/Milk Processor.fxml"));
-
-        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Nupur/Manage Dispatch Request.fxml"));
-
-        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/dairy/mahamud/Payment Management.fxml"));
-
-
-
-       // FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Nupur/Manage inventory level.fxml"));
-
-
-
-      //  FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Nupur/Inventory Manager Dashboard.fxml"));
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ParvezHassan/LogIn.fxml"));
-
-
-
-        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/dairy/ParvezHassan/LogIn.fxml"));
-
-        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/dairy/ParvezHassan/LogIn.fxml"));
-
->>>>>>> e3dda8649ee0b527c6dcb8433ed2b20a32b6eabd
-
 
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
